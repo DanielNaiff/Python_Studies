933dfffa87904d78bf83e748d3070325af4809b5
danielnaiff <daniel.naiff@yahoo.com.br>
2023-06-12 11:36:23 -0300

    Update ex062.py

--- Curso_do_Guanabara/Módulo 2/ex062.py
+++ Curso_do_Guanabara/Módulo 2/ex062.py
@@ -1,30 +1,16 @@
 primeiro_termo = int(input("Digite o primeiro termo:"))
 razao = int(input("Digite a razão: "))
 contador_primario = 0
-contador_geral = 0
 escolha = 1
-while contador_primario < 10:
-    termo = primeiro_termo + (contador_primario*razao)
-    print(f"{termo}-",end=" ")
-    contador_primario += 1
-print("PAUSA")
+total = 0
+escolha = 10
+
 while escolha != 0:
-
+    total = total + escolha
+    while contador_primario < total:
+        termo = primeiro_termo + (contador_primario*razao)
+        print(f"{termo}-",end=" ")
+        contador_primario += 1
+    print("PAUSA")
     escolha = int(input("\nQuantos termos voce quer amostrar a mais?"))
-    contador_secundario = 0
-    primeiro_termo = termo + razao
-
-    if escolha != 0: 
-
-        while contador_secundario < escolha :
-
-            termo = primeiro_termo + (contador_secundario*razao)
-            print(f"{termo}-",end=" ")
-            contador_secundario += 1
-
-        print("PAUSA")
-
-    contador_geral = contador_geral + contador_secundario
-contador_geral = contador_geral + contador_primario
-print("FIM")
-print("Total de numeros:", contador_geral)
+print(f"Prohrma finalizado com {total} itens escolhidos")
