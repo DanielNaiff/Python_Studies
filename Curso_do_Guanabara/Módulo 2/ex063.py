45fdf56d09d8a6ff95dbf58db9632c28717da83d
danielnaiff <daniel.naiff@yahoo.com.br>
2023-06-12 21:51:55 -0300

    Update ex063.py

--- Curso_do_Guanabara/Módulo 2/ex063.py
+++ Curso_do_Guanabara/Módulo 2/ex063.py
@@ -2,8 +2,8 @@ n = int(input("Quanto valores voce quer mostrar?"))
 contador = 0
 primeiro = 0
 segundo = 1
-
-while contador < n:
+print("0--1--",end='')
+while contador < n-2:
     soma = primeiro + segundo
     primeiro = segundo
     segundo = soma
