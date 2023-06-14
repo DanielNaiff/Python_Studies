e2ff4a77d7df36694ef46bddc15545b54481ae13
danielnaiff <daniel.naiff@yahoo.com.br>
2023-06-14 11:22:43 -0300

    Update ex068.py

--- Curso_do_Guanabara/Módulo 2/ex068.py
+++ Curso_do_Guanabara/Módulo 2/ex068.py
@@ -1,27 +1,25 @@
 from random import randint
 vitorias = 0
 while True:
-    escolha = str(input("Escolha se voce quer par ou ímpar [p/i]:")).lower().rstrip()
-    n = int(input("Digite um número [0 a 10]:"))
-    c = randint(0,10)
-    print('-='*30)
-    print(f'Sua escolha {n}, escolha do computador {c}')
-    print('-='*30)
-    if (n + c) % 2 == 0 and escolha == 'p':
-        vitorias += 1
-        print("Resultado par, vc venceu")
-        print('-='*30)
-    elif (n + c) % 2 == 0 and escolha == 'i':
-        print("Resultado par, voce perdeu")
-        print('-='*30)
-        break
-    elif (n + c) % 2 == 1 and escolha == 'i':
-        vitorias += 1
-        print("Resultado par, vc venceu")
-        print('-='*30)
-    elif (n + c) % 2 == 1 and escolha == 'p':
-        print("Resultado impar, voce perdeu")
-        print('-='*30)
-        break
-
-print("Vitórias consecutivas",vitorias)
+    jogador = int(input("Escolha um valor:"))
+    computador = randint(0,10)
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in 'pi':
+        tipo = str(input("Escolha se voce quer par ou imoar [p/i]")).lower().rstrip()[0]
+    print(f"Voce jogou {jogador} e o computador {computador}. Total de {total} ")
+    if tipo == 'p':
+        if total % 2 == 0:
+            print("Voce venceu!")
+            vitorias += 1
+        else:
+            print("Voce perdeu!")
+            break
+    elif tipo == 'i':
+        if total % 2 == 1:
+            print("Voce venceu!")
+            vitorias += 1
+        else:
+            print("Voce perdeu!")
+            break
+print("Total de vitórias:",vitorias)
