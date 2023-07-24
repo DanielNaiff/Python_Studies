3c0bbc12e9ad99a5c3c3d0e44394956ceda09490
danielnaiff <daniel.naiff@yahoo.com.br>
2023-07-23 23:06:47 -0300

    Create ex096.py

---
+++ Curso_do_Guanabara/Módulo 3/ex096.py
@@ -0,0 +1,10 @@
+def calculadora(l,c):
+    print("-="*25)
+    area = l*c
+    print(f"A área é {area}")
+    print("-="*25)
+
+
+l = float(input("Digite a largura:"))
+c = float(input("Digite o comprimento:"))
+calculadora(l,c)
